bfa38a563e4a0c9f587c20f43329b50a8c8a8204
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-11 05:26:11 +0545

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { PORT, HOST } from "./config/config.js";
 import connectDB from "./config/db.js";
 import bookRoutes from "./routes/bookRoutes.js";
@@ -10,6 +11,7 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Databse connection
 connectDB();
