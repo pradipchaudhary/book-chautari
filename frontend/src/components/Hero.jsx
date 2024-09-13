b3beee265ba256f453c37c4d70e85bb052ee2d4d
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-13 17:07:54 +0545

    Hero section design

---
+++ frontend/src/components/Hero.jsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+    return (
+        <section id="hero" className="hero">
+            <div className="container">
+                <div className="hero-content">
+                    <div className="hero-info">
+                        <h1>Hero Section </h1>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Hero;
