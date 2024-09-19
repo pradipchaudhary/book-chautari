dce82dc10e19be211583854b9903bc2e803148da
Pradip Chaudhary <chaudharypradip678@gmail.com>
2024-09-20 00:00:33 +0545

    login user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,11 @@
 import User from "../models/userModel.js";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 // @desc    Create a new user
 // @route   GET /api/user
 // @access  Public
+
+const JWT_SECRET = "Pradip";
 
 export const createUser = async (req, res) => {
     const { name, email, password, role } = req.body;
@@ -30,6 +33,34 @@ export const createUser = async (req, res) => {
             token,
         });
     } catch (error) {
+        res.status(500).json({ message: "Server error", error: error.message });
+    }
+};
+
+// @desc    Login a user
+// @route   GET /api/user
+// @access  Public
+export const loginUser = async (req, res) => {
+    const { email, password } = req.body;
+
+    try {
+        // Check if user exists
+        const user = await User.findOne({ email });
+        if (!user)
+            return res.status(400).json({ message: "Invalid credentials" });
+
+        // Compare passwords
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+            return res.status(400).json({ message: "Invalid credentials" });
+
+        // Generate JWT
+        const token = jwt.sign({ id: user._id, role: user.role }, JWT_SECRET, {
+            expiresIn: "1h",
+        });
+
+        res.status(200).json({ message: "Login successful", token });
+    } catch (err) {
         res.status(500).json({ message: "Server error", error: err.message });
     }
 };
